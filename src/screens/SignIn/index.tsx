7d834400bc6399cc54f4fe1804737d3352ecdb1b
Renan Loureiro <renanpl47@gmail.com>
2022-02-09 01:28:20 -0300

    feat: background gradient signIn Screen

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,7 +1,26 @@
 import React from "react"
+import { LinearGradient } from "expo-linear-gradient"
 
-import { Container } from "./styles"
+import { Container, Content, Form, Photo, Title } from "./styles"
+import { useTheme } from "styled-components/native"
+import { StatusBar } from "expo-status-bar"
+
+import SignInPhoto from "@assets/images/sign-in-image.png"
 
 export const SignIn = () => {
-  return <Container></Container>
+  const theme = useTheme()
+  return (
+    <Container>
+      <StatusBar style="light" translucent backgroundColor="transparent" />
+      <LinearGradient style={{ flex: 1 }} colors={theme.COLORS.GRADIENT}>
+        <Content>
+          <Photo resizeMode="contain" source={SignInPhoto} />
+
+          <Form>
+            <Title>Login</Title>
+          </Form>
+        </Content>
+      </LinearGradient>
+    </Container>
+  )
 }
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,5 +1,29 @@
+import { getStatusBarHeight } from "react-native-iphone-x-helper"
+import { RFValue } from "react-native-responsive-fontsize"
 import styled from "styled-components/native"
 
 export const Container = styled.View`
   flex: 1;
+`
+export const Content = styled.View`
+  flex: 1;
+  padding: 0 32px;
+  align-items: center;
+`
+
+export const Photo = styled.Image`
+  margin-top: ${getStatusBarHeight() + 50}px;
+  width: 100%;
+  height: 364px;
+`
+
+export const Form = styled.View`
+  width: 100%;
+  margin-top: 10px;
+`
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.COLORS.TITLE};
+  font-family: ${({ theme }) => theme.FONTS.TITLE};
+  font-size: ${RFValue(32)}px;
 `
