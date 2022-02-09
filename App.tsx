332c9be549d69d6422568d06e4e280fe0f307f04
Renan Loureiro <renanpl47@gmail.com>
2022-02-09 00:53:39 -0300

    build: add liner-gradient

--- App.tsx
+++ App.tsx
@@ -7,7 +7,9 @@ import { useFonts, DMSans_400Regular } from "@expo-google-fonts/dm-sans"
 import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display"
 
 import { ThemeProvider } from "styled-components/native"
-import theme from "@src/theme"
+import theme from "./src/theme"
+
+import { SignIn } from "@screens/SignIn"
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -20,7 +22,7 @@ export default function App() {
   }
   return (
     <ThemeProvider theme={theme}>
-      <View></View>
+      <SignIn />
     </ThemeProvider>
   )
 }
