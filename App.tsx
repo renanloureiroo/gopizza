aa1cca0a426900dc4de7bb68417d00fc4d7d6e46
Renan Loureiro <renanpl47@gmail.com>
2022-02-09 12:56:08 -0300

    chore: loading  inter font

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import AppLoading from "expo-app-loading"
 
 import { useFonts, DMSans_400Regular } from "@expo-google-fonts/dm-sans"
 import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display"
+import { Inter_500Medium } from "@expo-google-fonts/inter"
 
 import { ThemeProvider } from "styled-components/native"
 import theme from "./src/theme"
@@ -15,6 +16,7 @@ export default function App() {
   const [fontsLoaded] = useFonts({
     DMSans_400Regular,
     DMSerifDisplay_400Regular,
+    Inter_500Medium,
   })
 
   if (!fontsLoaded) {
