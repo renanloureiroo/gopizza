876bc59cd171cc6f9d6792adc9fe04cc2057929c
Renan Loureiro <renanpl47@gmail.com>
2022-02-10 20:20:16 -0300

    refactor: rehydrated user data of  async storage

--- src/contexts/auth/AuthContext.tsx
+++ src/contexts/auth/AuthContext.tsx
@@ -1,6 +1,7 @@
 import React, { createContext, ReactNode, useEffect, useState } from "react"
 import auth from "@react-native-firebase/auth"
 import firestore from "@react-native-firebase/firestore"
+import AsyncStorage from "@react-native-async-storage/async-storage"
 
 interface AuthContextData {
   user: User | null
@@ -23,10 +24,14 @@ interface User {
   isAdmin: boolean
 }
 
+const USER_COLLECTION = "@gopizza:users"
+
 export const AuthContext = createContext({} as AuthContextData)
 
 export const AuthProvider = ({ children }: AuthContextProps) => {
   const [user, setUser] = useState<User | null>(null)
+
+  const [isStarted, setIsStarted] = useState(false)
 
   async function signIn({ email, password }: SignInProps) {
     try {
@@ -47,11 +52,32 @@ export const AuthProvider = ({ children }: AuthContextProps) => {
         isAdmin,
       }
       console.log(userData)
+      await AsyncStorage.setItem("@gopizza:user", JSON.stringify(userData))
+
       setUser(userData)
     } catch (err) {
       throw new Error(err.code)
     }
   }
+
+  useEffect(() => {
+    setIsStarted(true)
+    const rehydrated = async () => {
+      if (isStarted) {
+        const userData = await AsyncStorage.getItem(USER_COLLECTION)
+
+        if (userData) {
+          const userFormatted = JSON.parse(userData) as User
+
+          setUser(userFormatted)
+        }
+      }
+    }
+
+    rehydrated()
+
+    return () => setIsStarted(false)
+  }, [])
 
   return (
     <AuthContext.Provider value={{ signIn, user }}>
